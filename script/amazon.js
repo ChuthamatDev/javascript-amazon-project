8b6cbfecc715415de0411ba394ae359c62797494
chuthamat <chuthamatbuaban@gmail.com>
2025-12-13 15:40:18 +0700

    Use products.js file

--- script/amazon.js
+++ script/amazon.js
@@ -1,7 +1,8 @@
-//Data Structure
+/* Data Structure - Generate HTML with JavaScript */
+/*
 const products = [
   {
-    iamge: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
       stars: 4.5,
@@ -10,7 +11,7 @@ const products = [
     priceCents: 1029,
   },
   {
-    iamge: "images/products/intermediate-composite-basketball.jpg",
+    image: "images/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
     rating: {
       stars: 4.5,
@@ -19,7 +20,7 @@ const products = [
     priceCents: 2095,
   },
   {
-    iamge: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
       stars: 4.5,
@@ -27,7 +28,35 @@ const products = [
     },
     priceCents: 799,
   },
+  {
+    image: "images/products/black-2-slot-toaster.jpg",
+    name: "2 Slot Toaster - Black",
+    rating: {
+      stars: 5,
+      count: 2197,
+    },
+    priceCents: 1899,
+  },
+  {
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
+    name: "6 Piece White Dinner Plate Set",
+    rating: {
+      stars: 4,
+      count: 37,
+    },
+    priceCents: 2067,
+  },
+  {
+    image: "images/products/6-piece-non-stick-baking-set.webp",
+    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    rating: {
+      stars: 4.5,
+      count: 175,
+    },
+    priceCents: 3499,
+  },
 ];
+*/
 
 let productsHTML = "";
 
@@ -37,7 +66,7 @@ products.forEach((product) => {
           <div class="product-image-container">
             <img
               class="product-image"
-              src="${product.iamge}"
+              src="${product.image}"
             />
           </div>
 
@@ -55,7 +84,7 @@ products.forEach((product) => {
             }</div>
           </div>
 
-          <div class="product-price">${(product.priceCents / 100).toFixed(
+          <div class="product-price">$${(product.priceCents / 100).toFixed(
             2
           )}</div>
 
